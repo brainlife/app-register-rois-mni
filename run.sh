c1137cb9de1416246c58ef6bd14ada39dbfb45b0
Brad Caron <bacaron245@gmail.com>
2022-09-23 17:09:28 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -30,7 +30,7 @@ done
 
 ## set if conditions
 [[ ${input_type} == 'T1' ]] && output_type='t1' || output_type='t2'
-[[ ${warp_to_use} == 'warp']] && warp_file="warp.nii.gz" || warp_file="inverse-warp.nii.gz"
+[[ ${warp_to_use} == 'warp' ]] && warp_file="warp.nii.gz" || warp_file="inverse-warp.nii.gz"
 
 ## set template for alignment
 case $TEMPLATE in
